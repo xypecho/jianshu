4a83f0ac6c36a38fd0db42335123af21d611cdae
xypecho <xypecho@163.com>
2019-05-15 23:18:14 +0800

    todolist demo

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: ['呆呆', 'sada']
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+  handleClick = () => {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+  render() {
+    return (
+      <Fragment>
+        <input type="text" onChange={this.handleChange} value={this.state.inputValue} />
+        <button onClick={this.handleClick}>click it</button>
+        {
+          this.state.list.map((item, key) => {
+            return <li key={key}>{item}</li>
+          })
+        }
+      </Fragment>
+    )
+  }
 }
-
-export default App;
