454f89d399504ebb0c45c92e10fad517badeae98
xypecho <xypecho@163.com>
2019-06-03 22:28:23 +0800

    完成header组件了哦

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import { HeaderWrapper, Logo } from './style';
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style';
 
 export default class Header extends Component {
     render() {
         return (
             <HeaderWrapper>
                 <Logo />
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>Aa</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className='writing'>写文章</Button>
+                    <Button className='register'>注册</Button>
+                </Addition>
             </HeaderWrapper>
         )
     }
--- jianshu/src/common/header/style.js
+++ jianshu/src/common/header/style.js
@@ -19,4 +19,78 @@ export const Logo = styled.a.attrs({
     padding: 0;
     background:url(${logoPic});
     background-size:contain;
-`
+`;
+
+export const Nav = styled.div`
+    width: 960px;
+    height: 100%;
+    margin: 0 auto;
+`;
+
+export const NavItem = styled.div`
+    line-height: 20px;
+    font-size: 17px;
+    padding: 17px 10px;
+    cursor: pointer;
+    &.left{
+        float:left;
+        color: #333;
+        margin-right: 10px;
+    }
+    &.right{
+        float:right;
+        color: #969696;
+        margin: 0 6px 0 10px;
+    }
+    &.active{
+        color: #ea6f5a;
+    }
+`;
+
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+})`
+    border: none;
+    outline: none;
+    padding: 0 40px 0 20px;
+    width: 160px;
+    height: 38px;
+    font-size: 14px;
+    border: 1px solid #eee;
+    border-radius: 40px;
+    background: #eee;
+    box-sizing:border-box;
+    margin-top: 9px;
+    padding-left: 15px;
+    &::placeholder{
+        color: #999;
+    }
+`;
+
+export const Addition = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 56px;
+`;
+
+export const Button = styled.div`
+    float: right;
+    width: 80px;
+    height: 40px;
+    line-height: 40px;
+    margin: 9px 5px 0 15px;
+    border: 1px solid rgba(236,97,73,.7);
+    border-radius: 20px;
+    font-size: 15px;
+    background-color: transparent;
+    text-align: center;
+    cursor: pointer;
+    &.register{
+        color: #ea6f5a;
+    }
+    &.writing{
+        color: #fff;
+        background-color: #ea6f5a;
+    }
+`;
