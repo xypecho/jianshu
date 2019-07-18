7e2e314a124e21b09ee7e0eabc4a81de49fc9dad
xypecho <xypecho@163.com>
2019-07-18 21:36:57 +0800

    完成详情页布局

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { DetailWrapper, Header, Content } from './style';
 
 export default class Detail extends Component {
     render() {
         return (
-            <div>details</div>
+            <DetailWrapper>
+                <Header>宿舍必备神器💕提升宿舍幸福感小物</Header>
+                <Content>
+                    <img src="https://upload-images.jianshu.io/upload_images/17001472-e56e82177b518090.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1000/format/webp" alt="" />
+                    <p>上了大学以后，宿舍就是我们的第二个家啦，所以一定要温馨且舒适，我经过接近一年的不断尝试踩雷…总结出这篇必备神器喔，图片上全部都是推荐的，文里还会提及一些踩雷的…</p>
+                    <p>上了大学以后，宿舍就是我们的第二个家啦，所以一定要温馨且舒适，我经过接近一年的不断尝试踩雷…总结出这篇必备神器喔，图片上全部都是推荐的，文里还会提及一些踩雷的…</p>
+                    <p>上了大学以后，宿舍就是我们的第二个家啦，所以一定要温馨且舒适，我经过接近一年的不断尝试踩雷…总结出这篇必备神器喔，图片上全部都是推荐的，文里还会提及一些踩雷的…</p>
+                    <p>上了大学以后，宿舍就是我们的第二个家啦，所以一定要温馨且舒适，我经过接近一年的不断尝试踩雷…总结出这篇必备神器喔，图片上全部都是推荐的，文里还会提及一些踩雷的…</p>
+                </Content>
+            </DetailWrapper>
         )
     }
 }
---
+++ jianshu/src/pages/detail/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const DetailWrapper = styled.div`
+    width:80%;
+    margin:0 auto;
+    padding-top: 20px;
+    padding-bottom: 40px;
+`
+
+export const Header = styled.div`
+    margin: 20px 0 0;
+    font-family: -apple-system,SF UI Display,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
+    font-size: 34px;
+    font-weight: 700;
+    line-height: 1.3;
+    text-align:center;
+`
+
+export const Content = styled.div`
+    color: #2f2f2f;
+    word-break: break-word!important;
+    word-break: break-all;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.7;
+    text-align:center;
+    img{
+        width:50%;
+        text-align:center;
+        margin:0 auto;
+    }
+    p{
+        text-align:left;
+    }
+`
